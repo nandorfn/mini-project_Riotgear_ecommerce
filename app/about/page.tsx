11550b755a5da5a707a342c429cc941607c3eb82
nandorfn <firnandorfn@gmail.com>
2023-09-29 23:38:52 +0700

    add: about page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+
+const page: React.FC = () => {
+    return (
+        <>
+          <article className="max-w-lg text-justify mx-auto">
+            <p>Welcome to RIOTGEAR, where fashion meets rebellion in the heart of the urban landscape. Born from a passion for self-expression and a love for street culture, RIOTGEAR is more than just clothing; it&apos;s a statement of defiance and individuality. Our journey began on the gritty streets, and we&apos;ve since evolved into a global force, uniting skaters, artists, and rebels of all kinds under one banner.</p>
+            <p>At RIOTGEAR, we believe that fashion is a canvas for personal stories. Our designs are a fusion of raw energy and artistic expression, drawing inspiration from the pulse of the city and the spirit of subcultures. Each piece in our collection is crafted with precision and an unwavering commitment to quality. From graphic tees that speak volumes to comfortable yet stylish skatewear, RIOTGEAR is your armor of choice for conquering the concrete jungle.</p>
+            <p>But RIOTGEAR is more than just a clothing brand; it&apos;s a community. We&apos;re a tribe of individuals who refuse to conform to the norm, who skate to the beat of their own drum, and who wear their rebellion proudly. Our mission is to empower you to be unapologetically yourself. Join us in this riotous journey, and let&apos;s make a mark on the world, one fearless step at a time.</p>
+          </article>
+        </>
+    );
+};
+
+export default page;
--- app/components/Body/Body.tsx
+++ app/components/Body/Body.tsx
@@ -6,39 +6,29 @@ import skateBg from '../../assets/Content/Skate.png'
 import shoesBg from '../../assets/Content/Shoes.png'
 
 const Body: React.FC = () => {
-    return (
-        <>
-          <section className="p-4 gap-4 mt-4  grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-          </section>
-          <section className="p-4 gap-3 grid grid-cols-1 sm:grid-cols-2">
-            <CategoryCard 
-              imgSrc={shirtBg}
-              label="Top"
-            />
-            <CategoryCard 
-              imgSrc={jacketBg}
-              label="Outwear"
-            />
-            <CategoryCard 
-              imgSrc={skateBg}
-              label="Bottom"
-            />
-            <CategoryCard 
-              imgSrc={shoesBg}
-              label="Accessories"
-            />
-
-          </section>
-        </>
-    );
+  return (
+    <>
+      <section className="p-4 gap-4 mt-4  grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </section>
+      <section className="mt-8">
+      <h1 className="text-3xl p-4 font-medium md:text-5xl">Upgrade Your Style with Our Streetwear Collection</h1>
+      <div className="p-4 gap-3 grid grid-cols-1 sm:grid-cols-2">
+          <CategoryCard imgSrc={shirtBg} label="Top" />
+          <CategoryCard imgSrc={jacketBg} label="Outwear" />
+          <CategoryCard imgSrc={skateBg} label="Bottom" />
+          <CategoryCard imgSrc={shoesBg} label="Accessories" />
+      </div>
+      </section>
+    </>
+  );
 };
 
 export default Body;
