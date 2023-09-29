bf3973eba345b304670743b4c097f9f923063093
nandorfn <firnandorfn@gmail.com>
2023-09-29 22:01:41 +0700

    update: footer margin

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,38 +8,40 @@ import Link from 'next/link'
 
 const Footer: React.FC = () => {
   const menus = [
-    {label: 'Terms of Service', link:'/tos'},
-    {label: 'Privacy Policy', link:'/privacy-policy'},
-    {label: 'Cookie Policy', link:'/cookie-policy'},
-    {label: 'Partners', link:'/partners'},
+    { label: 'Terms of Service', link: '/tos' },
+    { label: 'Privacy Policy', link: '/privacy-policy' },
+    { label: 'Cookie Policy', link: '/cookie-policy' },
+    { label: 'Partners', link: '/partners' },
   ]
 
   return (
     <>
-      <figure className='flex justify-between pt-4 border-t-2 mt-20 mb-3'>
-        <Image
-          src={RiotLogo}
-          alt='RiotGear icon'
-        />
-        <div className='flex gap-3'>
-          <Image src={fbIcon} alt='Facebook icon'/>
-          <Image src={xIcon} alt='Instagram icon'/>
-          <Image src={igIcon} alt='Instagram icon'/>
-          <Image src={ttIcon} alt='Instagram icon'/>
+      <footer className='mx-4'>
+        <figure className='flex justify-between pt-4 border-t-2 mt-20 mb-3'>
+          <Image
+            src={RiotLogo}
+            alt='RiotGear icon'
+          />
+          <div className='flex gap-3'>
+            <Image src={fbIcon} alt='Facebook icon' />
+            <Image src={xIcon} alt='Instagram icon' />
+            <Image src={igIcon} alt='Instagram icon' />
+            <Image src={ttIcon} alt='Instagram icon' />
+          </div>
+        </figure>
+        <div className='flex flex-col md:flex-row items-center  md:justify-between text-base text-[#9FA0A2] mb-8'>
+          <span>
+            &copy;2023 RIOTGEAR. All Right reserved
+          </span>
+          <ul className='flex gap-3'>
+            {menus?.map((menu, index) =>
+              <li key={index}>
+                <Link className=' hover:text-base-300' href={menu.link}>{menu.label}</Link>
+              </li>
+            )}
+          </ul>
         </div>
-      </figure>
-      <div className='flex justify-between text-base text-[#9FA0A2] mb-8'>
-        <span>
-          &copy;2023 RIOTGEAR. All Right reserved
-        </span>
-        <ul className='flex gap-3'>
-          {menus?.map((menu, index) =>
-            <li key={index}>
-              <Link href={menu.link}>{menu.label}</Link>
-            </li>
-          )}
-        </ul>
-      </div>
+      </footer>
     </>
   );
 };
