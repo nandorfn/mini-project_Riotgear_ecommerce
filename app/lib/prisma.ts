e19877daca55de694fba654b75af5d5a0afddd3a
nandorfn <firnandorfn@gmail.com>
2023-10-26 16:45:41 +0700

    update: prisma generated

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from '@/prisma/generated/client/index';
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
